cb5c07b24d6ecc51bb76cb268be65b905f22b936
Stefan Jørgensen <sjarge13@student.aau.dk>
2017-09-25 21:16:42 +1300

    Cleaning project and seperating code into partials and pages
    the index.ejs are now seperated intro multiple files, so code can be reused for other pages

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +19,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Public resources
 app.use(express.static(path.join(__dirname, 'public')));
 
+// All pages need to be catched here
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
